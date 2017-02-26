01e5bf631d5f24af4eadf24951423f462ed79bdb
James Stewart <jstewart101@gmail.com>
2017-02-26 16:44:46 +1100

    Drop PLA preheat temp to 190

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -936,7 +936,7 @@
 // @section temperature
 
 // Preheat Constants
-#define PREHEAT_1_TEMP_HOTEND 210
+#define PREHEAT_1_TEMP_HOTEND 190
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
